447c668e1e1c0ce3d12fe2b41353fe2be80e172e
JacobV321 <102176988+JacobV321@users.noreply.github.com>
2024-03-15 14:06:16 -0600

    Update App.js

--- storyIA_front/src/App.js
+++ storyIA_front/src/App.js
@@ -11,7 +11,7 @@ const RenAIssanceComponent = () => {
 
   const handleSubmit = async () => {
     try {
-      const res = await axios.post('http://localhost:3001/api/ask', { question }); // Cambia la URL según tu configuración de backend
+      const res = await axios.post('https://jacobdominio.me:3001/api/ask', { question }); // Cambia la URL según tu configuración de backend
       setResponse(res.data.answer);
     } catch (error) {
       console.error(error);
@@ -21,7 +21,7 @@ const RenAIssanceComponent = () => {
   };
 
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px', backgroundImage: `url(${require('./images/rnc.jpeg')})`, backgroundSize: 'cover', minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+    <div style={{ textAlign: 'center', background: `url(${require('./images/rnc.jpeg')}) center center fixed`, backgroundSize: 'cover', minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <div style={{ border: '3px solid #3f51b5', borderRadius: '4px', padding: '22px', maxWidth: '500px', margin: 'auto', backgroundColor: 'rgba(255, 255, 255, 0.8)' }}>
         <h2 style={{ color: '#3f51b5', marginBottom: '20px' }}>RenAIssance</h2>
         <p style={{ marginBottom: '10px' }}>Ask something to a deceased renaissance painter </p>
@@ -29,7 +29,7 @@ const RenAIssanceComponent = () => {
           value={question}
           onChange={handleChange}
           placeholder=""
-          style={{ width: '100%', minHeight: '100px', marginBottom: '10px', borderRadius: '5px', padding: '1px' }}
+          style={{ width: '100%', minHeight: '100px', marginBottom: '10px', borderRadius: '5px', padding: '1px', resize: 'none' }}
         />
         <br />
         <button onClick={handleSubmit} style={{ backgroundColor: '#3f51b5', color: 'white', padding: '10px 20px', borderRadius: '5px', border: 'none', cursor: 'pointer' }}>Ask</button>
@@ -42,5 +42,5 @@ const RenAIssanceComponent = () => {
   );
 };
 
-
 export default RenAIssanceComponent;
+
